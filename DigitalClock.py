e687a3f22fe0e2d38e8326e5ea7ce8b5be83b61b
praveen <s170599@rguktsklm.ac.in>
2021-09-07 20:54:40 +0530

    added a welcome message

--- DigitalClock.py
+++ DigitalClock.py
@@ -4,5 +4,8 @@ window=Tk()
 
 window.title("Digital clock")
 window.geometry("400x400")
+window.configure(bg="cyan")
 
+label=Label(window,text="welcome" ,font=("Arial Black",78,"bold") ,bg="steelblue")
+label.pack(pady=100)
 window.mainloop()
